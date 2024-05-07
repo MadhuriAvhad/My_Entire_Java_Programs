9d347d9db7d2733384b488369b0324158632eeec
Madhuri Avhad <74188070+MadhuriAvhad@users.noreply.github.com>
2024-05-07 10:33:14 +0530

    Update and rename Assignment10_AreaOfSurfaceCircumferenceUsingFinalVariable.java to AreaOfSurfaceCircumferenceUsingFinalVariable.java

--- AreaOfSurfaceCircumferenceUsingFinalVariable.java
+++ AreaOfSurfaceCircumferenceUsingFinalVariable.java
@@ -1,6 +1,6 @@
 package javaByMKT;
 
-public class Assignment10_AreaOfSurfaceCircumferenceUsingFinalVariable {
+public class AreaOfSurfaceCircumferenceUsingFinalVariable {
 	
 	final double pi = 3.14159;
 	final static int r = 5;
@@ -8,7 +8,7 @@ public class Assignment10_AreaOfSurfaceCircumferenceUsingFinalVariable {
 
 	public static void main(String[] args) {
 		
-		Assignment10_AreaOfSurfaceCircumferenceUsingFinalVariable c = new Assignment10_AreaOfSurfaceCircumferenceUsingFinalVariable();
+		AreaOfSurfaceCircumferenceUsingFinalVariable c = new AreaOfSurfaceCircumferenceUsingFinalVariable();
 		
 		System.out.println("Area of Circle for radius 5 : "+(c.pi*(r*r)));
 		System.out.println("Circumference of Cirecle for radius 5 : "+(2*c.pi*r));
