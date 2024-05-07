e91948619549e987e3c77761e5deb2c3d0a8fe6e
Madhuri Avhad <74188070+MadhuriAvhad@users.noreply.github.com>
2024-05-07 10:37:06 +0530

    Update OrderOfSIBMMIIBConstructor.java

--- OrderOfSIBMMIIBConstructor.java
+++ OrderOfSIBMMIIBConstructor.java
@@ -2,7 +2,7 @@ package javaByMKT;
 
 public class OrderOfSIBMMIIBConstructor {
 	
-	Assignment13_OrderOfSIBMMIIBConstructor()
+	OrderOfSIBMMIIBConstructor()
 	{
 		System.out.println("Started with the Constructor execution");
 	}
@@ -10,7 +10,7 @@ public class OrderOfSIBMMIIBConstructor {
 	public static void main(String[] args) {
 		
 		System.out.println("Started with the Main Method execution");
-		Assignment13_OrderOfSIBMMIIBConstructor order = new Assignment13_OrderOfSIBMMIIBConstructor();
+		OrderOfSIBMMIIBConstructor order = new OrderOfSIBMMIIBConstructor();
 	}
 	
 	{
